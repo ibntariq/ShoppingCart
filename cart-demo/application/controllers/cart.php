90be0add900c671449a7d7d33f7b84ee0d9e5ade
Omer Tariq <ibntariq@live.com>
2013-07-02 21:07:46 +0500

    Shopping Cart using Codeigniter

---
+++ cart-demo/application/controllers/cart.php
@@ -0,0 +1,36 @@
+<?php
+
+    class Cart extends CI_Controller{
+        
+        function index(){
+                        
+            $this->load->model('cart_model');
+            $data['items'] = $this->cart_model->get_products();
+            $this->load->view('products',$data);
+            
+        }
+        
+        function insert(){
+            
+            $this->load->model('cart_model');
+            $product = $this->cart_model->insert_product($this->input->post('id'));
+            $item = array(
+                'id'=> $this->input->post('id'),
+                'qty'=>$this->input->post('qunatity'),
+                'price'=> $product->price,
+                'name'=>$product->name                
+            );
+            $this->cart->insert($item);
+            redirect('cart');
+            
+        }
+        
+        
+        
+    }
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
